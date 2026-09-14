59fd153f541b53da6ed31e2ed5d3b3fff98f8d51
velonica0 <like@mail.nankai.edu.cn>
2026-09-13 18:26:26 -0700

    Allow cross-compiling to linux-riscv64

--- recipe/build-r-base.sh
+++ recipe/build-r-base.sh
@@ -37,7 +37,7 @@ if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 1 ]]; then
     export r_cv_prog_fc_cc_compat_complex=yes
     export r_cv_zdotu_is_usable=yes
 
-    if [[ "${target_platform}" == "osx-arm64" || "${target_platform}" == "linux-aarch64" || "${target_platform}" == "linux-ppc64le" ]]; then
+    if [[ "${target_platform}" == "osx-arm64" || "${target_platform}" == "linux-aarch64" || "${target_platform}" == "linux-ppc64le" || "${target_platform}" == "linux-riscv64" ]]; then
       export r_cv_func_calloc_works=yes
       export r_cv_func_isfinite_works=yes
       export r_cv_func_log1p_works=yes
@@ -53,7 +53,7 @@ if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 1 ]]; then
 
     if [[ "${target_platform}" == "osx-arm64"  ]]; then
       export r_cv_putenv_unset=no
-    elif [[ "${target_platform}" == "linux-aarch64" || "${target_platform}" == "linux-ppc64le" ]]; then
+    elif [[ "${target_platform}" == "linux-aarch64" || "${target_platform}" == "linux-ppc64le" || "${target_platform}" == "linux-riscv64" ]]; then
       export r_cv_putenv_unset=yes
     else
       echo "Unknown cross compiling architecture"
